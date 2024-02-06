880bfb3af51c5a8404c272fefe30342d48d5eeb3
Andrey Polnikov <polnikov.andrey@gmail.com>
2024-02-06 16:29:20 +0300

    + applications

--- services/back/src/main/resources/db/script/setup.sql
+++ services/back/src/main/resources/db/script/setup.sql
@@ -8,9 +8,7 @@ drop user if exists sb3_react_user;
 create user sb3_react_user encrypted password 'password';
 
 alter database sb3_react owner to sb3_react_admin;
-grant all on database sb3_react to sb3_react_admin;
+grant connect on database sb3_react to sb3_react_user;
+\c sb3_react
 grant usage on schema public to sb3_react_user;
-
--- grant usage on schema public to sb3_react_user;
--- grant select, insert, update, delete on all tables in schema public to sb3_react_user;
--- alter default privileges for user sb3_react_user in schema public grant select, insert, update, delete on tables to sb3_react_user;
+alter default privileges for user sb3_react_admin in schema public grant select, insert, update, delete on tables to sb3_react_user;
